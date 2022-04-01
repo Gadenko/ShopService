e85a70ed956ba54f503b91e6bd34e5f538a3d8a1
Gadenko <gadenko@gmx.net>
2022-04-01 16:42:16 +0200

    NewTry

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -1,5 +1,57 @@
 package Model;
 
 public class Product {
+    public String name;
+    public int id;
+    public double price;
 
+    public Product(String name, int id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        if (Double.compare(product.price, price) != 0) return false;
+        return name != null ? name.equals(product.name) : product.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + id;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/Model/ProductRepo.java
+++ src/main/java/Model/ProductRepo.java
@@ -1,7 +1,16 @@
 package Model;
 
-import java.util.Objects;
-
 public class ProductRepo {
+
+    public static Product game1 = new Product("Destroy the World 1",1,49.99);
+    public static Product game2 = new Product("Destroy the World Eternity AddOn",2,20.99);
+    public static Product game3 = new Product("MelloWorld",3,15.99);
+    public static Product game4 = new Product("Piggy Farmer 21",4,59.99);
+    public static Product game5 = new Product("Water Simulator 2",5,39.99);
+    public static Product game6 = new Product("Snow Simulator",6,29.99);
+    public static Product game7 = new Product("Make it, like a GOD",7,20.01);
+    public static Product game8 = new Product("Pick up your dream Car",8,23.99);
+    public static Product game9 = new Product("Hello Mars",9,40.99);
+
 
 }
